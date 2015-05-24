0603e6d87866d276ab32354cdefe52c2bc56bd37
Nimalan1 <n.govender@me.com>
2015-05-24 20:56:33 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+# this function creates a list with 4 member functions: set, get, setInv,getInv                                 # getInv uses <<- assignment operator so that these internal variables are unexposed to the outside environment. 
 
-## Write a short comment describing this function
+  makeCacheMatrix <- function(x = matrix()) {
 
-makeCacheMatrix <- function(x = matrix()) {
+      xinv <- NULL 
+      set <- function(y) {
+	  x <<- y
+	  xinv <<- NULL 
+      }
 
-}
+      get <- function() x 
+      setInv <- function(inv) xinv <<- inv # set the inversed matrix
+      getInv <- function() xinv # return the inversed matrix
+      list(set = set, get = get,
+	       setInv = setInv,
+	       getInv = getInv)
+  }
 
 
-## Write a short comment describing this function
+  cacheSolve <- function(x, ...) {
+      m <- x$getInv()  x
+      if(!is.null(m)) {
+	  message("getting cached data")
+	  return(m)
+      }
+      data <- x$get() 
+      m <- solve(data) 
+      x$setInv(m)
+      m 
+  }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+ # Test
+ # generate a  matrix
+  test <- matrix(runif(9,1,100),3,3)
+  testCached <- makeCacheMatrix(test)
+
+  testInv <- cacheSolve(testCached)
+  testInv <- cacheSolve(testCached)
+  testInv <- cacheSolve(testCached)
+  testInv <- cacheSolve(testCached)
+  testInv <- cacheSolve(testCached)
